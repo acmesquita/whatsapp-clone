af9f1a0e59117f3b038ac9b93d5be7a64ce5b70b
Catharina Mesquita <anacatharina@infoway-pi.com.br>
2019-07-15 18:09:40 -0300

    Inserindo Emoji

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -235,10 +235,22 @@ class WhatsAppController{
 
         this.el.panelEmojis.querySelectorAll('.emojik').forEach(emoji => {
             emoji.on('click', e=> {
-                console.log(emoji.dataset.unicode)
+                let img = this.el.imgEmojiDefault.cloneNode()
+
+                img.style.cssText = emoji.style.cssText
+                img.dataset.unicode = emoji.dataset.unicode
+                img.alt = emoji.dataset.unicode
+
+                emoji.classList.forEach(name => {
+                    img.classList.add(name)
+                })
+
+                this.el.inputText.appendChild(img)
+                this.el.inputText.dispatchEvent(new Event('keyup'));
             })
         })
 
+        
     }
 
     startRecordMicrophoneTime(){
