9cfb6d387ecdd6f785c55aa48128c63d707c913a
Catharina Mesquita <anacatharina@infoway-pi.com.br>
2019-07-15 17:09:36 -0300

    Project

---
+++ src/util/Format.js
@@ -0,0 +1,9 @@
+class Format{
+
+    static getCamelCase(text){
+        let div = document.createElement('div')
+        div.innerHTML = `<div data-${text}="id"></div>`;
+        return Object.keys(div.firstChild.dataset)[0];
+    }
+    
+}
