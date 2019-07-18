5ce11a5023c0807af18931da7bf7caa2411a9201
Catharina Mesquita <anacatharina@infoway-pi.com.br>
2019-07-18 18:38:11 -0300

    Atualizando dados do perfil do Usuário

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -160,7 +160,11 @@ export class WhatsAppController{
         })
 
         this.el.btnSavePanelEditProfile.on('click', e => {
-            console.log(this.el.inputNamePanelEditProfile.innerHTML)
+            this.el.btnSavePanelEditProfile.disabled = true;
+            this._user.name = this.el.inputNamePanelEditProfile.innerHTML
+            this._user.save().then(()=>{
+                this.el.btnSavePanelEditProfile.disabled = false;
+            });
         })
 
         this.el.formPanelAddContact.on('submit', e => {
