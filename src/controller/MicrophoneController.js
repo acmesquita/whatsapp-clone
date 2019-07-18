dca5a9847063f04fb5a2116cedf66cbcc00a9915
Catharina Mesquita <anacatharina@infoway-pi.com.br>
2019-07-18 15:13:23 -0300

    Gravando áudio capturando do microfone com MediaRecord

--- src/controller/MicrophoneController.js
+++ src/controller/MicrophoneController.js
@@ -3,14 +3,17 @@ import { ClassEvent } from "../util/ClassEvent";
 export class MicrophoneController extends ClassEvent{
     constructor(){
         super();
+        this._available = false;
+        this._mimeType = 'audio/webm'
         navigator.mediaDevices.getUserMedia({
             audio: true
         }).then(stream => {
+            this._available = true;
+
             this._stream = stream;
             let audio = new Audio();
-            audio.srcObject = stream
-            audio.play();
-            this.trigger('play', audio)
+            
+            this.trigger('ready', this._stream)
         }).catch(err =>{
             console.error(err);
         });
@@ -20,5 +23,50 @@ export class MicrophoneController extends ClassEvent{
         this._stream.getTracks().forEach(track => {
             track.stop();
         })
+    }
+
+    startRecorder(){
+        if(this.isAvailable()){
+            this._mediaRecorder = new MediaRecorder(this._stream, {
+                mimeType: this._mimeType
+            })
+
+            this._recordedChunks = []
+
+            this._mediaRecorder.addEventListener('dataavailable', e => {
+                if(e.data.size > 0) this._recordedChunks.push(e.data);
+            });
+
+            this._mediaRecorder.addEventListener('stop', e=>{
+                let blob = new Blob(this._recordedChunks, {
+                    type: this._mimeType
+                })
+                let fileName = `rec${Date.now().toString()}.webm`
+                let file = new File([blob], fileName, {
+                    type: this._mimeType,
+                    lastModified: Date.now()
+                });
+
+                let reader = new FileReader();
+                reader.onload = e => {
+                    let audio = new Audio(reader.result);
+                    audio.play()
+                }
+                reader.readAsDataURL(file)
+            })
+
+            this._mediaRecorder.start();
+        }
+    }
+
+    stopRecorder(){
+        if(this.isAvailable()){
+            this._mediaRecorder.stop();
+            this.stop();
+        }
+    }
+
+    isAvailable(){
+        return this._available;
     }
 }
--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -280,18 +280,18 @@ export class WhatsAppController{
             
             this._microphoneController = new MicrophoneController();
 
-            this._microphoneController.on('play', audio=>{
-                console.log('recebi o evento play', audio)
+            this._microphoneController.on('ready', audio=>{
+                this._microphoneController.startRecorder()
             })
         })
 
         this.el.btnCancelMicrophone.on('click', e => {
-            this._microphoneController.stop()
+            this._microphoneController.stopRecorder()
             this.closeRecordMicrophone()
         })
 
         this.el.btnFinishMicrophone.on('click', e => {
-            this._microphoneController.stop()
+            this._microphoneController.stopRecorder()
             this.closeRecordMicrophone()
         })
 
