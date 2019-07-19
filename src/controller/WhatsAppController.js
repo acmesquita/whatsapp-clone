564c8182f327bea288ccbda4c22fe97aac0c7839
Catharina Mesquita <anacatharina@infoway-pi.com.br>
2019-07-19 14:25:53 -0300

    Evento de click um contato

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -116,6 +116,22 @@ export class WhatsAppController{
                     img.show()
                 }
 
+                div.on('click', e=>{
+                    this.el.activeName.innerHTML = contact.name
+                    this.el.activeStatus.innerHTML = contact.status
+                    if(contact.photo){
+                        let img = this.el.activePhoto;
+                        img.src = contact.photo;
+                        img.show();
+                    }
+
+                    this.el.home.hide();
+                    this.el.main.css({
+                        display: 'flex'
+                    })
+
+                });
+
                 this.el.contactsMessagesList.appendChild(div);
             })
         })
