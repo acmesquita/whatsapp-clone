b93dc7befc11c8d4a41e912d23a51687a6bbd2eb
Catharina Mesquita <anacatharina@infoway-pi.com.br>
2019-07-26 16:55:46 -0300

    Atualizando status de mensagem (wait, send e read)

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -572,16 +572,24 @@ export class WhatsAppController{
             docs.forEach(doc => {
                 let data = doc.data()
                 data.id = doc.id
-                if (!this.el.panelMessagesContainer.querySelector('#_'+data.id)){
-                    
+                let message = new Message()
+                message.fromJSON(data);
+                let me = (data.from === this._user.email)
 
-                    let message = new Message()
-                    message.fromJSON(data);
-                    let me = (data.from === this._user.email)
+                if (!this.el.panelMessagesContainer.querySelector('#_'+data.id)){
+                    if (!me){
+                        doc.ref.set({
+                            status: 'read'
+                        }, {
+                            merge: true
+                        })
+                    }
                     let view = message.getViewElement(me);
                     this.el.panelMessagesContainer.appendChild(view);
-
-                    
+                   
+                }else if(me){
+                    let msgEl = this.el.panelMessagesContainer.querySelector('#_'+data.id);
+                    msgEl.querySelector('.message-status').innerHTML = message.getStatusViewElement().outerHTML;
                 }
                 
             })
--- src/model/Message.js
+++ src/model/Message.js
@@ -55,7 +55,7 @@ export class Message extends Model{
                             </div>
                             <div class="_3a5-b">
                                 <div class="_1DZAH" role="button">
-                                    <span class="message-time">17:01</span>
+                                    <span class="message-time">${Format.timeStampToTime(this.timeStamp)}</span>
                                     <div class="message-status">
                                         <span data-icon="msg-dblcheck">
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 15" width="16" height="15">
@@ -108,7 +108,7 @@ export class Message extends Model{
                         </div>
                         <div class="_3Lj_s">
                             <div class="_1DZAH" role="button">
-                                <span class="message-time">18:56</span>
+                                <span class="message-time">${Format.timeStampToTime(this.timeStamp)}</span>
                                 <div class="message-status">
                                     <span data-icon="msg-time">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 15" width="16" height="15">
@@ -154,9 +154,9 @@ export class Message extends Model{
                             </div>
                             <div class="_2TvOE">
                                 <div class="_1DZAH text-white" role="button">
-                                    <span class="message-time">17:22</span>
+                                    <span class="message-time">${Format.timeStampToTime(this.timeStamp)}</span>
                                     <div class="message-status">
-                                        <span data-icon="msg-check-light">
+                                        <span data-icon="msg-check">
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 15" width="16" height="15">
                                                 <path fill="#FFF" d="M10.91 3.316l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.879a.32.32 0 0 1-.484.033L1.891 7.769a.366.366 0 0 0-.515.006l-.423.433a.364.364 0 0 0 .006.514l3.258 3.185c.143.14.361.125.484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z"></path>
                                             </svg>
@@ -241,7 +241,7 @@ export class Message extends Model{
                         </div>
                         <div class="_27K_5">
                             <div class="_1DZAH" role="button">
-                                <span class="message-time">17:48</span>
+                                <span class="message-time">${Format.timeStampToTime(this.timeStamp)}</span>
                                 <div class="message-status">
                                     <span data-icon="msg-dblcheck-ack">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 15" width="16" height="15">
@@ -274,7 +274,7 @@ export class Message extends Model{
                         </div>
                         <div class="_2f-RV">
                             <div class="_1DZAH">
-                                <span class="msg-time">${Format.timeStampToTime(this.timeStamp)}</span>
+                                <span class="message-time">${Format.timeStampToTime(this.timeStamp)}</span>
                             </div>
                         </div>
                     </div>
@@ -284,19 +284,80 @@ export class Message extends Model{
                 break;
         }
 
-        let className = (me) ? 'message-out': 'message-in';
+        let className = 'message-in';
+
+        if(me){
+            className = 'message-out';
+            div.querySelector('.message-time').parentElement.appendChild(this.getStatusViewElement());
+        }
+
         div.firstElementChild.classList.add(className)
         return div;
     }
 
+    getStatusViewElement(){
+        let div = document.createElement('div')
+        div.className = 'message-status';
+
+        switch(this.status){
+            case 'wait':
+                div.innerHTML = `
+                <span data-icon="msg-time">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 15" width="16" height="15">
+                        <path fill="#859479" d="M9.75 7.713H8.244V5.359a.5.5 0 0 0-.5-.5H7.65a.5.5 0 0 0-.5.5v2.947a.5.5 0 0 0 .5.5h.094l.003-.001.003.002h2a.5.5 0 0 0 .5-.5v-.094a.5.5 0 0 0-.5-.5zm0-5.263h-3.5c-1.82 0-3.3 1.48-3.3 3.3v3.5c0 1.82 1.48 3.3 3.3 3.3h3.5c1.82 0 3.3-1.48 3.3-3.3v-3.5c0-1.82-1.48-3.3-3.3-3.3zm2 6.8a2 2 0 0 1-2 2h-3.5a2 2 0 0 1-2-2v-3.5a2 2 0 0 1 2-2h3.5a2 2 0 0 1 2 2v3.5z"></path>
+                    </svg>
+                </span>
+                `;
+                break;
+            case 'sent':
+                div.innerHTML = `
+                <span data-icon="msg-check">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 15" width="16" height="15">
+                        <path fill="#4FC3F7" d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.879a.32.32 0 0 1-.484.033l-.358-.325a.319.319 0 0 0-.484.032l-.378.483a.418.418 0 0 0 .036.541l1.32 1.266c.143.14.361.125.484-.033l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.879a.32.32 0 0 1-.484.033L1.891 7.769a.366.366 0 0 0-.515.006l-.423.433a.364.364 0 0 0 .006.514l3.258 3.185c.143.14.361.125.484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z"></path>
+                    </svg>
+                </span>
+                `
+                break;
+            case 'received':
+                div.innerHTML = `
+                <span data-icon="msg-dblcheck">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 15" width="16" height="15">
+                        <path fill="#FFF" d="M10.91 3.316l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.879a.32.32 0 0 1-.484.033L1.891 7.769a.366.366 0 0 0-.515.006l-.423.433a.364.364 0 0 0 .006.514l3.258 3.185c.143.14.361.125.484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z"></path>
+                    </svg>
+                </span>
+                `;
+                break;
+            case 'read':
+                div.innerHTML = `
+                <span data-icon="msg-dblcheck-ack">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 15" width="16" height="15">
+                        <path fill="#4FC3F7" d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.879a.32.32 0 0 1-.484.033l-.358-.325a.319.319 0 0 0-.484.032l-.378.483a.418.418 0 0 0 .036.541l1.32 1.266c.143.14.361.125.484-.033l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.879a.32.32 0 0 1-.484.033L1.891 7.769a.366.366 0 0 0-.515.006l-.423.433a.364.364 0 0 0 .006.514l3.258 3.185c.143.14.361.125.484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z"></path>
+                    </svg>
+                </span>
+                `;
+                break;  
+        }
+        return div;
+    }
+
     static send(chatId, from, type, content){
-        return Message.getRef(chatId).add({
-            content,
-            timeStamp: new Date(),
-            status: 'wait',
-            type,
-            from
+        return new Promise((resolve, reject)=>{
+            Message.getRef(chatId).add({
+                content,
+                timeStamp: new Date(),
+                status: 'wait',
+                type,
+                from
+            }).then(result => {
+                result.parent.doc(result.id).set({
+                    status: 'sent'
+                }, {
+                    merge: true
+                }).then(()=> resolve());
+            });
+
         });
+        
     }
 
     static getRef(chatId){
